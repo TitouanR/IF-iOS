ca59f96d1a917937021bd1f911ca50c85ee5c5e3
TitouanR <titouan.rossier@gmail.com>
2013-10-22 21:53:46 -0400

    Initial Commit

---
+++ Imaginarium Festival/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Imaginarium Festival
+//
+//  Created by Titouan Rossier on 22/10/13.
+//  Copyright (c) 2013 TitouanRossier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IFAppDelegate class]));
+    }
+}
